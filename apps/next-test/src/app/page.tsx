1b9cce49963caef30931ac05add413604533a899
derrick <iga00257@gmail.com>
2024-02-04 22:41:19 +0800

    feat: add next-test basic repo

---
+++ apps/next-test/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Button from '../../../../packages/button/src/button';
+
+export default function Home() {
+  return (
+    <div>
+       <Button/>
+    </div>
+
+  );
+}
