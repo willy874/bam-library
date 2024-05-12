9325b918309008113f776a2c4b07f278c526cdf0
hc-psy <austenpsy@gmail.com>
2024-05-12 21:00:20 +0800

    fix: create_pnpm syntax

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,11 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
-NC='\033[0m'
+NC='\033[0m' # No Color
 PWD=$(pwd)
 TARGET_NVM_VERSION="0.39.0"
 TARGET_NODE_VERSION="20.11.0"
-TARGET_PNPM_VERSION="8.15.1"
+TARGET_PNPM_VERSION="8.15.1" # "9.1.0"
 
 function create_nvm() {
   if ! [ -s "$HOME/.nvm/nvm.sh" ]; then
@@ -33,10 +33,17 @@ function create_nodejs() {
 }
 
 function create_pnpm() {
-  PNPM_VERSION=$(pnpm -v)
-  if ! [ -s "$HOME/.nvm/versions/node/v$TARGET_NODE_VERSION/bin/pnpm"] && [[ $PNPM_VERSION =~ $TARGET_PNPM_VERSION ]]; then
-    npm install -g pnpm@$TARGET_PNPM_VERSION
+  # Chech if pnpm is installed and version is correct
+  if ! command -v pnpm >/dev/null 2>&1 || ! [[ $(pnpm -v) =~ $TARGET_PNPM_VERSION ]]; then
+    echo -e "Install pnpm: ${BLUE}$TARGET_PNPM_VERSION${NC}"
+    npm install -g pnpm@$TARGET_PNPM_VERSION --verbose
   fi
+
+  # Print pnpm version
+  echo "PNPM Version: $(pnpm -v)"
+  # Print pnpm path
+  echo "PNPM Path: $(which pnpm)"
+
   pnpm reinstall
 }
 
