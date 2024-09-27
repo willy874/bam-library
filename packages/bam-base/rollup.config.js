516eba55b7ce2ef1474d62f058e9f5cc5ba6ca02
pakerchang <pakerchang@gmail.com>
2024-09-27 14:41:25 +0800

    feat: Add rollup setup.

---
+++ packages/bam-base/rollup.config.js
@@ -0,0 +1,16 @@
+const { componentBuilder } = require('@bam/builder');
+const { dts } = require('rollup-plugin-dts');
+
+module.exports = componentBuilder({
+  rollupConfiguration: [
+    { external: [/^react\/*/] },
+    {
+      input: 'src/main.ts',
+      output: {
+        file: 'dist/types/main.d.ts',
+        format: 'esm',
+      },
+      plugins: [dts()],
+    },
+  ],
+});
